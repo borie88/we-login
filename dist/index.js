a6fdaec1dda8c1d38158f581e5f4d3ef4ec24a9f
borie88 <gpb8977@163.com>
2019-06-25 20:49:45 +0800

    refactor poor code
    async functions return promises so no need to wrap with a promise =(

--- dist/index.js
+++ dist/index.js
@@ -10,33 +10,21 @@ class Welogin {
     this.appSecret = appSecret
   }
 
-  getUserInfo (code, encryptedData, iv) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const { session_key } = await this.loginUser(code)
-        const pc = new WechatCrypt(this.appId, session_key)
-        const userData = await pc.decryptData(encryptedData, iv)
-        resolve(userData)
-      } catch (error) {
-        reject(error)
-      }
-    })
+  async getUserInfo (code, encryptedData, iv) {
+    const { session_key } = await this.loginUser(code)
+    const pc = new WechatCrypt(this.appId, session_key)
+    const userData = await pc.decryptData(encryptedData, iv)
+    return userData
   }
 
-  loginUser (code) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        if (!code) throw Error('no code provided')
-        const url = `https://api.weixin.qq.com/sns/jscode2session?appid=${this.appId}&secret=${this.appSecret}&js_code=${code}&grant_type=authorization_code`
-        const res = await Axios.get(url)
-        const { errcode, errmsg, ...loginData } = res.data
-        // <Int> errorcode === 0 means success
-        if (errcode) throw Error(errmsg)
-        resolve(loginData)
-      } catch (error) {
-        reject(error)
-      }
-    })
+  async loginUser (code) {
+    if (!code) throw Error('no code provided')
+    const url = `https://api.weixin.qq.com/sns/jscode2session?appid=${this.appId}&secret=${this.appSecret}&js_code=${code}&grant_type=authorization_code`
+    const res = await Axios.get(url)
+    const { errcode, errmsg, ...loginData } = res.data
+    // <Int> errorcode === 0 means success
+    if (errcode) throw Error(errmsg)
+    return loginData
   }
 
 }
